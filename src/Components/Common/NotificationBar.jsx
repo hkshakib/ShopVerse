90affcba5930adfd2ba0f3295208147f8b4ff056
hkshakib <humayunkibriashakib@gmail.com>
2023-06-06 12:02:07 +0600

    feat: Install MUI and MUI-Icons, Starting transform all code to MUI

--- src/Components/Common/NotificationBar.jsx
+++ src/Components/Common/NotificationBar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Classes from './../../Styles/Common/NotificationBar.module.css';
-
+import {AiOutlinePhone} from  "react-icons/ai";
+import { BsChevronDown } from "react-icons/bs";
 
 const NotificationBar = () => {
   return (
     <div className={Classes.NotificationBar}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, similique.</p>
+      <div className={Classes.PhoneNumber}> 
+        <AiOutlinePhone/> +88-01710889028
+      </div>
+
+      <div className={Classes.Offer}>
+        <div className={Classes.Discount}> Get 50% Discount</div>
+        <div className={Classes.ShopNow}>Shop Now</div>
+      </div>
+      <div className={Classes.Location}>
+        <div className={Classes.Language}>
+          English <BsChevronDown/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Classes from './../../Styles/Pages/Home.module.css';
-
+import Bannar from './../../Assets/OfferBanner.jpg';
 function Home() {
+
   return (
-    <div className={Classes.Home}></div>    
+    <div className={Classes.Home}>
+      <div className={Classes.Bannar}>
+        <img src={Bannar} alt="" />
+      </div>
+      
+      <div className={Classes.ProductShowCase}>
+
+      </div>
+    </div>    
   )
 }
 
-export default Home
+export default Home;
